b6bcb0af73df4157908a2b756c652da83fa95cc6
ankeli-laptop <ankeli@cs.stonybrook.edu>
2015-02-23 16:56:41 -0500

    finished a* algo and one heuristic

--- project_1/peg_solitaire_sol.py
+++ project_1/peg_solitaire_sol.py
@@ -10,34 +10,41 @@ size = 7
 
 
 class Tree(object):
-    def __init__(self, data, parent, move):
+    def __init__(self, data, parent, move, g):
         self.data = data
         self.parent = parent
         self.children = {}
         self.move = move
         self.dead = False
+        self.g = g
+        self.h = 1000
 
 
 def init():
     l = lambda: defaultdict(l)
     board = l()
+    opt = l()
     for i in range(0, size):
         for k in range(0, size):
             if (0 <= i <= 1 or 5 <= i <= 6) and (0 <= k <= 1 or 5 <= k <= 6):
                 board[i][k] = '-'
+                opt[i][k] = '-'
             else:
+                # board[i][k] = 'x'
                 board[i][k] = '0'
+                opt[i][k] = '0'
 
     board[1][3] = board[2][2] = board[2][3] = board[2][4] = board[3][3] = board[4][3] = 'x'
-    #board[3][3] = '0'
+    # board[3][3] = '0'
+    opt[3][3] = 'x'
     print 'Initial board configuration:'
     draw_board(board)
 
-    return board
+    return board, opt
 
 
 def draw_board(board):
-    #print '\n'
+    # print '\n'
     for i in range(0, size):
         for k in range(0, size):
             sys.stdout.write(str(board[i][k]) + ' ')
@@ -60,7 +67,7 @@ def get_next_move(board):
                 if (board[i][k + 1] == 'x') and (board[i][k + 2] == '0'):
                     next_move.append([(i, k), (i, k + 2)])
 
-    #print next_move
+    # print next_move
     return next_move
 
 
@@ -79,6 +86,15 @@ def check_finish(board):
     return True
 
 
+def cut_edge(node):
+    count = 0
+    for i in range(0, len(node.children)):
+        if (len(get_next_move(node.children[i].data)) == 0) and (node.children[i].data[3][3] != 'x'):
+            node.children[i].dead = True
+            count += 1
+    print ('Cut %d edge(s)!' % count)
+
+
 def iddfs(node, depth):
     if depth == 0:
         if check_finish(node.data):
@@ -90,7 +106,7 @@ def iddfs(node, depth):
         if len(next_move) > 0:
             i = 0
             for move in next_move:
-                node.children[i] = Tree(copy.deepcopy(node.data), node, move)
+                node.children[i] = Tree(copy.deepcopy(node.data), node, move, 0)
                 node.children[i].data[move[0][0]][move[0][1]] = '0'
                 node.children[i].data[move[1][0]][move[1][1]] = 'x'
                 node.children[i].data[(move[0][0] + move[1][0]) / 2][(move[0][1] + move[1][1]) / 2] = '0'
@@ -106,11 +122,83 @@ def iddfs(node, depth):
             return False, node.move
 
 
-def main():
-    board = init()
-    root = Tree(board, None, None)
-    depth = 0
+def heuristic_1(node, opt):
+    h = 0
+    for i in range(0, size):
+        for k in range(0, size):
+            if node.data[i][k] != opt[i][k]:
+                h += 1
+    print 'Heuristic 1 result: ', h
+    return h
 
+
+def heuristic_2(node, opt):
+    h = 0
+    for i in range(0, size):
+        for k in range(0, size):
+            if node.data[i][k] == 'x':
+                h += (abs(i-3) + abs(k-3))
+    print 'Heuristic 2 result: ', h
+    return h
+
+
+def a_star_search(node, opt, depth):
+    if check_finish(node.data):
+        return True
+    else:
+        next_move = get_next_move(node.data)
+        if len(next_move) > 0:
+            i = 0
+            depth += 1  # Initialize g value for all children
+            for move in next_move:
+                node.children[i] = Tree(copy.deepcopy(node.data), node, move, depth)
+                node.children[i].data[move[0][0]][move[0][1]] = '0'
+                node.children[i].data[move[1][0]][move[1][1]] = 'x'
+                node.children[i].data[(move[0][0] + move[1][0]) / 2][(move[0][1] + move[1][1]) / 2] = '0'
+                node.children[i].h = heuristic_2(node.children[i], opt)
+                draw_board(node.children[i].data)
+                print 'Child ' + str(i) + ' initialized!'
+                i += 1
+
+            print 'Number of children: ', len(node.children)
+            cut_edge(node)
+
+            while True:
+                tmp_cost = 100
+                chosen = 0
+                for i in range(0, len(node.children)):
+                    # print ("%d: %d" % (i, node.children[i].h))
+                    # print ("Child %d is dead? %r" % (i, node.children[i].dead))
+                    if not node.children[i].dead:
+                        if node.children[i].h <= tmp_cost:
+                            chosen = i
+                            tmp_cost = node.children[i].h
+
+                print 'Choosing: child ', chosen
+                draw_board(node.children[chosen].data)
+                result = a_star_search(node.children[chosen], opt, depth)
+                if not result:
+                    # print 'Dead end! Number of children before delete: ', len(node.children)
+                    # del node.children[chosen]
+                    # print 'Dead end! Number of children: ', len(node.children)
+                    node.children[chosen].dead = True
+                    if len(node.children) == 0:
+                        return False
+                else:
+                    return result
+
+        else:
+            node.dead = True
+            print 'No next move, return false!'
+            return False
+
+
+def main():
+    board, opt = init()
+    # draw_board(opt)
+    root = Tree(board, None, None, 0)
+    depth = 0
+    '''
     while True:
         result, move_seq = iddfs(root, depth)
         if result:
@@ -120,6 +208,9 @@ def main():
 
     print '\nBelow is memory usage for Iterative Deepening Search:\n'
     print hpy().heap()
+    '''
+    print '\nNow we do A* search using heuristic 1'
+    print a_star_search(root, opt, 0)
 
 
 main()
